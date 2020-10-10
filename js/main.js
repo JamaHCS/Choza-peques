099c6caed14bf939a90d46eb27be5cb5db3dae2d
JamaHCS <jama117@hotmail.com>
2020-10-09 23:18:41 -0500

    bus

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+const validateComeback = () => {
+  let aBoolean = confirm('¿Seguro de que quieres regresar al inicio?');
+  if (aBoolean) {
+    window.location.href = window.location.origin + '/index.html';
+  }
+};
+
+$('#modal-container').on('shown.bs.modal', function () {
+  $('#modal').trigger('focus');
+});
+
+// window.onload = function () {
+//   $('#modal-container').modal('show');
+// };
